3d92892ef3b8aea6e58656597640af3004a1e319
Mir Carvalho <deppbrazil@gmail.com>
2021-06-02 20:10:00 -0300

    :rocket: Add button in form

--- HelloWord/Form1.cs
+++ HelloWord/Form1.cs
@@ -17,5 +17,9 @@ namespace HelloWord
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
     }
 }
